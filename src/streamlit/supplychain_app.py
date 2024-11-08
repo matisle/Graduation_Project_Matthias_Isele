982f622bb6ff637ef13ab47dfd7bc07a3387986c
matisle <139568134+matisle@users.noreply.github.com>
2024-11-08 23:24:33 +0100

    Update

--- src/streamlit/supplychain_app.py
+++ src/streamlit/supplychain_app.py
@@ -14,7 +14,7 @@ page=st.sidebar.radio("Learn more about ...", pages)
 #st.sidebar.title("Authors")
 #st.sidebar.link_button("Stefanie Arlt", "https://www.linkedin.com/in/stefanie-arlt-augsburg-germany/?locale=en_US")
 #st.sidebar.link_button("Matthias Isele", "https://www.linkedin.com/in/mat-isle-24a665270/")
-st.sidebar.subheader('Matthias Isele')
+st.sidebar.title('Matthias Isele')
 st.sidebar.subheader('iselema@gmx.de')
 st.sidebar.subheader('+49 160 94978140')
 
